2c6b50ac479633b51ef1af209886463f529809e3
Miguel Angel Mateo <miguizindex@gmail.com>
2023-05-19 11:23:52 -0400

    nice

--- src/components/Paginate/Paginate.jsx
+++ src/components/Paginate/Paginate.jsx
@@ -25,7 +25,9 @@ const Paginate = ({
     );
     await setSearch(json.data);
   }
+  //Al otogar un nombre por medio del input actualiza la barra de navegacion para que coincida con la misma ruta y se pueda cambiar de la pagina
 
+  // Al cambiar de pagina de manera general se ejecuta esta funcion
   async function handlePageChange(page) {
     setCurrentPage(page);
     let json = await axios.get(
@@ -33,12 +35,17 @@ const Paginate = ({
     );
     await setShows(json.data);
   }
+  // Al cambiar de pagina de manera general se ejecuta esta funcion
 
   return (
     <div className="lg:px-[10rem]  sm:max-lg:px-[5rem] py-10">
       {searchShows.length !== 0 ? (
         <ResponsivePagination
-          className="lg:w-3/5 w-full  flex items-center justify-between border-t border-gray-200"
+          className="lg:w-3/5 w-full  flex flex-row items-center justify-between mx-auto border-t border-gray-200"
+          pageLinkClassName=" "
+          pageItemClassName="text-lg p-2 font-medium leading-none cursor-pointer text-gray-600 hover:text-indigo-700 border-t border-transparent "
+          activeItemClassName="border-madder border-t-2 text-madder"
+          navClassName="hover:text-madder text-[35px] py-auto  text-lg p-2 flex items-center text-gray-600 hover:text-indigo-700 cursor-pointer"
           current={currentPage}
           total={totalPagesSearch}
           onPageChange={(page) => handlePageChangeSearch(page)}
@@ -48,8 +55,8 @@ const Paginate = ({
           className="lg:w-3/5 w-full  flex flex-row items-center justify-between mx-auto border-t border-gray-200"
           pageLinkClassName=" "
           pageItemClassName="text-lg p-2 font-medium leading-none cursor-pointer text-gray-600 hover:text-indigo-700 border-t border-transparent "
-          activeItemClassName="border-madder border-t-2"
-          navClassName="hover:text-madder text-[30px] py-auto  text-lg p-2 flex items-center text-gray-600 hover:text-indigo-700 cursor-pointer"
+          activeItemClassName="border-madder border-t-2 text-madder"
+          navClassName="hover:text-madder text-[35px] py-auto  text-lg p-2 flex items-center text-gray-600 hover:text-indigo-700 cursor-pointer"
           current={currentPage}
           total={totalPages}
           disabledItemClassName
